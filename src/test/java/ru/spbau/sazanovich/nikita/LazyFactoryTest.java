7c2f7346633c7ceb748cacbc692983c96600218c
niksaz <nikitasazanovich@gmail.com>
2017-02-23 14:34:33 +0300

    single threaded tests are finished

--- src/test/java/ru/spbau/sazanovich/nikita/LazyFactoryTest.java
+++ src/test/java/ru/spbau/sazanovich/nikita/LazyFactoryTest.java
@@ -5,16 +5,18 @@ import org.junit.Test;
 import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 public class LazyFactoryTest {
 
-    private static final Supplier<Long> SUMMER_OF_FIRST_NUMBERS = () -> {
+    private static final Supplier<Long> SUMMER_OF_NUMBERS = () -> {
         long sum = 0;
         for (int i = 0; i <= 1000000000; i++) {
             sum += i;
         }
         return sum;
     };
+    private static final Supplier<Object> NULLER = () -> null;
 
     /**
      * Tests basic properties: correctness of the result, returning of the same object each time,
@@ -22,25 +24,25 @@ public class LazyFactoryTest {
      */
 
     @Test
-    public void createSingleThreadedLazySimpleCase() {
-        testOnSimpleCase(LazyFactory.createSingleThreadedLazy(new LimitedSupplier<>(SUMMER_OF_FIRST_NUMBERS, 1)));
+    public void testSingleThreadedLazySimpleCase() {
+        testOnSimpleCase(LazyFactory.createSingleThreadedLazy(new LimitedSupplier<>(SUMMER_OF_NUMBERS, 1)));
     }
 
     @Test
-    public void createMultiThreadedLazySimpleCase() {
-        testOnSimpleCase(LazyFactory.createMultiThreadedLazy(new LimitedSupplier<>(SUMMER_OF_FIRST_NUMBERS, 1)));
+    public void testMultiThreadedLazySimpleCase() {
+        testOnSimpleCase(LazyFactory.createMultiThreadedLazy(new LimitedSupplier<>(SUMMER_OF_NUMBERS, 1)));
     }
 
     @Test
-    public void createLockFreeLazySimpleCase() {
-        testOnSimpleCase(LazyFactory.createLockFreeLazy(new LimitedSupplier<>(SUMMER_OF_FIRST_NUMBERS, 1)));
+    public void testLockFreeLazySimpleCase() {
+        testOnSimpleCase(LazyFactory.createLockFreeLazy(new LimitedSupplier<>(SUMMER_OF_NUMBERS, 1)));
     }
 
     private void testOnSimpleCase(Lazy<Long> lazy) {
         final Long result = lazy.get();
-        assertEquals(SUMMER_OF_FIRST_NUMBERS.get().longValue(), result.longValue());
+        assertEquals(SUMMER_OF_NUMBERS.get(), result);
         final Long anotherResult = lazy.get();
-        assertEquals(result, anotherResult);
+        assertSame(result, anotherResult);
     }
 
     /**
@@ -48,18 +50,44 @@ public class LazyFactoryTest {
      */
 
     @Test
-    public void createSingleThreadedLazyBottom() {
-        LazyFactory.createSingleThreadedLazy(new LimitedSupplier<>(SUMMER_OF_FIRST_NUMBERS, 0));
+    public void testSingleThreadedLazyBottom() {
+        LazyFactory.createSingleThreadedLazy(new LimitedSupplier<>(SUMMER_OF_NUMBERS, 0));
     }
 
     @Test
-    public void createMultiThreadedLazyBottom() {
-        LazyFactory.createMultiThreadedLazy(new LimitedSupplier<>(SUMMER_OF_FIRST_NUMBERS, 0));
+    public void testMultiThreadedLazyBottom() {
+        LazyFactory.createMultiThreadedLazy(new LimitedSupplier<>(SUMMER_OF_NUMBERS, 0));
     }
 
     @Test
-    public void createLockFreeLazyBottom() {
-        LazyFactory.createLockFreeLazy(new LimitedSupplier<>(SUMMER_OF_FIRST_NUMBERS, 0));
+    public void testLockFreeLazyBottom() {
+        LazyFactory.createLockFreeLazy(new LimitedSupplier<>(SUMMER_OF_NUMBERS, 0));
+    }
+
+    /**
+     * Tests whether null suppliers are handled correctly.
+     */
+
+    @Test
+    public void testSingleThreadedLazyNull() {
+        testOnNull(LazyFactory.createSingleThreadedLazy(new LimitedSupplier<>(NULLER, 1)));
+    }
+
+    @Test
+    public void testMultiThreadedLazyNull() {
+        testOnNull(LazyFactory.createMultiThreadedLazy(new LimitedSupplier<>(NULLER, 1)));
+    }
+
+    @Test
+    public void testLockFreeLazyNull() {
+        testOnNull(LazyFactory.createLockFreeLazy(new LimitedSupplier<>(NULLER, 1)));
+    }
+
+    private <T> void testOnNull(Lazy<T> lazy) {
+        final T result = lazy.get();
+        assertEquals(null, result);
+        final T anotherResult = lazy.get();
+        assertSame(result, anotherResult);
     }
 
     private static class LimitedSupplier<T> implements Supplier<T> {
