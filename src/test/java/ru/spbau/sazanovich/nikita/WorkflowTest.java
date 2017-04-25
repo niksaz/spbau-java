5a35142a9e7e28c77c46be11331e7cdfe7abae9c
niksaz <nikitasazanovich@gmail.com>
2017-04-25 16:17:34 +0300

    workflow test extended

--- src/test/java/ru/spbau/sazanovich/nikita/WorkflowTest.java
+++ src/test/java/ru/spbau/sazanovich/nikita/WorkflowTest.java
@@ -8,8 +8,10 @@ import ru.spbau.sazanovich.nikita.server.Server;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Random;
 
 import static java.lang.Thread.sleep;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -18,7 +20,7 @@ public class WorkflowTest {
     private static final int TEST_PORT = 50000;
 
     @Test
-    public void testList() throws Exception {
+    public void testListAndGet() throws Exception {
         Server server = new Server(TEST_PORT);
         server.start();
         sleep(500);
@@ -28,16 +30,24 @@ public class WorkflowTest {
         Path file1 = folder.newFile("file1").toPath();
         Path file2 = folder.newFile("file2").toPath();
         Path dir = folder.newFolder("dir").toPath();
-        String text = "A selectable channel for stream-oriented connecting sockets.";
-        Files.write(file1, text.getBytes());
+        byte[] data = new byte[10000];
+        new Random().nextBytes(data);
+        Files.write(file1, data);
 
         Client client = new Client(TEST_PORT);
+
         List<String> list = client.list(folder.getRoot().getAbsolutePath());
         assertTrue(list != null);
         assertEquals(3, list.size());
         assertTrue(list.contains(file1.getFileName().toString()));
         assertTrue(list.contains(file2.getFileName().toString()));
         assertTrue(list.contains(dir.getFileName().toString()));
+
+        Path tmp = folder.newFile("tmp").toPath();
+        boolean successful = client.get(file1.toAbsolutePath().toString(), tmp.toAbsolutePath().toString());
+        assertTrue(successful);
+        byte[] dataReceived = Files.readAllBytes(tmp);
+        assertArrayEquals(data, dataReceived);
 
         server.stop();
     }
